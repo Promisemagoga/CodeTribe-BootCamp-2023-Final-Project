7743bf688734f3beccd53ab9377fa0bd15758c36
Promise Tshegofatso magoga <104593732+Promisemagoga@users.noreply.github.com>
2023-02-26 16:16:53 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    const typo = "Join our community";
+document.getElementById("cont1").innerHTML = typo;
+
+const typo1 = "30-day, hassle-free money back guarantee"
+document.getElementById("typo1").innerHTML = typo1;
+
+const typo2 = "Gain access to our full library of tutorials along with expert code reviews.<br> Perfect for any developers who are serious about honing their skills."
+document.getElementById("typo2").innerHTML = typo2;
+
+//monthly subscription
+const typo3 = "Monthly Subscription"
+document.getElementById("typo3").innerHTML = typo3;
+
+const typo4 = "<span class='dollar'>&dollar;29</span><span class='month'> per month</span>";
+document.getElementById("typo4").innerHTML = typo4;
+
+const typo5 = "Full access for less than &dollar;1 a day";
+document.getElementById("typo5").innerHTML = typo5;
+
+//why us
+const typo6 = "Why Us";
+document.getElementById("typo6").innerHTML = typo6;
+
+const typo7 = " Tutorials by industry experts Peer &amp; expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week"
+document.getElementById("typo7").innerHTML = typo7;
+}
